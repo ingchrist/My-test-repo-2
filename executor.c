3465bfc79c3905f9cefbe67442b4670c26bdc9ed
labaski <topejames234@gmail.com>
2023-09-19 07:23:54 -0700

    files added

--- executor.c
+++ executor.c
@@ -5,32 +5,33 @@
  *      * @argv: array of tokens, ie. argument vectors
  *        * @linkedlist_path: PATH in LL form
  *          * Return: 0 on success, -1 on failure
- *            */
+ */
 void executor(char *argv[], env_t *linkedlist_path)
 {
-		pid_t child_status = 0;
-			char *abs_path;
+	pid_t child_status = 0;
+	char *abs_path;
 
-				abs_path = search_os(argv[0], linkedlist_path);
-					if (!abs_path)
-							{
-										perror("command not found\n");
-												return;
-													}
+	abs_path = search_os(argv[0], linkedlist_path);
 
-						switch (child_status = fork())
-								{
-										case -1:
-													perror("fork failed\n");
-															break;
-																case 0:
-																	if (execve(abs_path, argv, environ) == -1)
-																					perror("execution failed\n");
-																			break;
-																				default:
-																					free(abs_path);
-																							if (wait(NULL) == -1)
-																											perror("wait failed\n");
-																									break;
-																										}
+	if (!abs_path)
+	{
+		perror("command not found\n");
+		return;
+	}
+
+	switch (child_status = fork())
+	{
+		case -1:
+			perror("fork failed\n");
+			break;
+		case 0:
+			if (execve(abs_path, argv, environ) == -1)
+				perror("execution failed\n");
+			break;
+		default:
+			free(abs_path);
+			if (wait(NULL) == -1)
+				perror("wait failed\n");
+			break;
+	}
 }
